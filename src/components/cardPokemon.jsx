f6c0fbe01f1bda547647562c6d4c998d4a2e4e48
vaniav3 <vania.vergara@.mail.udp.cl>
2023-01-15 23:09:08 -0300

    card

--- src/components/cardPokemon.jsx
+++ src/components/cardPokemon.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../css/styles.css'
 
 export default function CardPokemon(props){
     return (
@@ -7,7 +8,7 @@ export default function CardPokemon(props){
                 {props.pokemons.map(pokemon => {
                     return (
                         <div className='card' key={pokemon.id}>
-                            <img className='img' src={pokemon.image} alt={pokemon.name} />
+                            <img className='card-img' src={pokemon.image} alt={pokemon.name} />
                             <div className='card-text'>
                                 <h2 >{pokemon.name}</h2>
                                 <p>Weight: {pokemon.weight}</p>
